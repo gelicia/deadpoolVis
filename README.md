83a06c5882716e22b337bd22047e7236290a68c0
gelicia <kdurivage@gelicia.com>
2014-03-08 16:53:22 -0600

    look at comic data, sort smartly

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 Replace configTEMP.json with config.json and fill with info. Visualization is meant for one superhero - ID is to stop multiple matches. Find your superhero's ID using the api call tester on developer.marvel.com
 
-todo
-- find stories
-- find series
+["onsaleDate", "focDate", "unlimitedDate", "digitalPurchaseDate"] 
+all comics have a onsaledate
---
+++ js/main.js
@@ -0,0 +1,34 @@
+d3.selection.prototype.moveToFront = function() {
+  return this.each(function(){
+    this.parentNode.appendChild(this);
+  });
+};
+//["onsaleDate", "focDate", "unlimitedDate", "digitalPurchaseDate"] 
+function loadComics(){
+	d3.json('./data/comics.json', function(err, data){
+		data.sort(function(a,b){
+			var aSaleDate = _.find(a.dates, function(d){return d.type == 'onsaleDate';});
+			var bSaleDate = _.find(b.dates, function(d){return d.type == 'onsaleDate';});
+
+			//sort on saleDate, if that is not valid then by digitalPurchaseDate, then by unlimitedDate
+			aSaleDate = aSaleDate.date.substring(0,1) == "-" ? _.find(a.dates, function(d){return d.type == 'digitalPurchaseDate';}) : aSaleDate;
+			if(aSaleDate === undefined || aSaleDate.date.substring(0,1) == "-"){
+				aSaleDate = _.find(a.dates, function(d){return d.type == 'unlimitedDate';});
+			}
+			bSaleDate = bSaleDate.date.substring(0,1) == "-" ? _.find(b.dates, function(d){return d.type == 'digitalPurchaseDate';}) : bSaleDate;
+			if(bSaleDate === undefined || bSaleDate.date.substring(0,1) == "-"){
+				bSaleDate = _.find(b.dates, function(d){return d.type == 'unlimitedDate';});
+			}
+
+			if (aSaleDate.date > bSaleDate.date){
+				return 1;
+			}
+			else if (aSaleDate.date < bSaleDate.date){
+				return -1;
+			}
+			else { return 0;}
+		});
+
+		console.log(data);
+	});
+}
